e5dc51a26da7e71c0cf3a330909cf154b12fdb71
Skyuzi05 <mister.skyuzi@yandex.ru>
2019-07-29 15:14:04 +0300

    -Добавленно решение капчи -Испралвены мелкие баги

---
+++ AutoRuParser/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace AutoRuParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbDilers = new System.Windows.Forms.CheckBox();
+            this.cbUsed = new System.Windows.Forms.CheckBox();
+            this.cbNew = new System.Windows.Forms.CheckBox();
+            this.cbAll = new System.Windows.Forms.CheckBox();
+            this.cbCredit = new System.Windows.Forms.CheckBox();
+            this.cbHistory = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbDilers);
+            this.groupBox1.Controls.Add(this.cbUsed);
+            this.groupBox1.Controls.Add(this.cbNew);
+            this.groupBox1.Controls.Add(this.cbAll);
+            this.groupBox1.Controls.Add(this.cbCredit);
+            this.groupBox1.Controls.Add(this.cbHistory);
+            this.groupBox1.Location = new System.Drawing.Point(4, 445);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фильтры";
+            // 
+            // cbDilers
+            // 
+            this.cbDilers.AutoSize = true;
+            this.cbDilers.Enabled = false;
+            this.cbDilers.Location = new System.Drawing.Point(108, 67);
+            this.cbDilers.Name = "cbDilers";
+            this.cbDilers.Size = new System.Drawing.Size(68, 17);
+            this.cbDilers.TabIndex = 5;
+            this.cbDilers.Text = "Дилеры";
+            this.cbDilers.UseVisualStyleBackColor = true;
+            // 
+            // cbUsed
+            // 
+            this.cbUsed.AutoSize = true;
+            this.cbUsed.Location = new System.Drawing.Point(6, 67);
+            this.cbUsed.Name = "cbUsed";
+            this.cbUsed.Size = new System.Drawing.Size(89, 17);
+            this.cbUsed.TabIndex = 4;
+            this.cbUsed.Text = "С пробегом";
+            this.cbUsed.UseVisualStyleBackColor = true;
+            this.cbUsed.CheckedChanged += new System.EventHandler(this.CbUsed_CheckedChanged);
+            // 
+            // cbNew
+            // 
+            this.cbNew.AutoSize = true;
+            this.cbNew.Location = new System.Drawing.Point(6, 44);
+            this.cbNew.Name = "cbNew";
+            this.cbNew.Size = new System.Drawing.Size(61, 17);
+            this.cbNew.TabIndex = 3;
+            this.cbNew.Text = "Новые";
+            this.cbNew.UseVisualStyleBackColor = true;
+            this.cbNew.CheckedChanged += new System.EventHandler(this.CbNew_CheckedChanged);
+            // 
+            // cbAll
+            // 
+            this.cbAll.AutoSize = true;
+            this.cbAll.Location = new System.Drawing.Point(6, 21);
+            this.cbAll.Name = "cbAll";
+            this.cbAll.Size = new System.Drawing.Size(43, 17);
+            this.cbAll.TabIndex = 2;
+            this.cbAll.Text = "Все";
+            this.cbAll.UseVisualStyleBackColor = true;
+            this.cbAll.CheckedChanged += new System.EventHandler(this.CbAll_CheckedChanged);
+            // 
+            // cbCredit
+            // 
+            this.cbCredit.AutoSize = true;
+            this.cbCredit.Enabled = false;
+            this.cbCredit.Location = new System.Drawing.Point(108, 44);
+            this.cbCredit.Name = "cbCredit";
+            this.cbCredit.Size = new System.Drawing.Size(73, 17);
+            this.cbCredit.TabIndex = 1;
+            this.cbCredit.Text = "В кредит";
+            this.cbCredit.UseVisualStyleBackColor = true;
+            // 
+            // cbHistory
+            // 
+            this.cbHistory.AutoSize = true;
+            this.cbHistory.Enabled = false;
+            this.cbHistory.Location = new System.Drawing.Point(108, 21);
+            this.cbHistory.Name = "cbHistory";
+            this.cbHistory.Size = new System.Drawing.Size(88, 17);
+            this.cbHistory.TabIndex = 0;
+            this.cbHistory.Text = "С историей";
+            this.cbHistory.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbLog);
+            this.groupBox2.Location = new System.Drawing.Point(4, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(784, 441);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Лог";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(6, 15);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(772, 420);
+            this.rtbLog.TabIndex = 0;
+            this.rtbLog.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnClearLog);
+            this.groupBox3.Controls.Add(this.btnStop);
+            this.groupBox3.Controls.Add(this.btnStart);
+            this.groupBox3.Location = new System.Drawing.Point(221, 445);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(567, 94);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Меню";
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(401, 17);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(160, 65);
+            this.btnClearLog.TabIndex = 2;
+            this.btnClearLog.Text = "Очистить лог";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.BtnClearLog_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(6, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(389, 30);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(6, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(389, 30);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 540);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Парсер для Auto.ru";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbDilers;
+        private System.Windows.Forms.CheckBox cbUsed;
+        private System.Windows.Forms.CheckBox cbNew;
+        private System.Windows.Forms.CheckBox cbAll;
+        private System.Windows.Forms.CheckBox cbCredit;
+        private System.Windows.Forms.CheckBox cbHistory;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ AutoRuParser/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+using CsvExports;
+using System.Net;
+
+namespace AutoRuParser
+{
+    public partial class Form1 : Form
+    {
+        #region//Настройки интерфейса
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CbNew_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbNew.Checked == true)
+            {
+                cbHistory.Enabled = false;
+                cbCredit.Enabled = false;
+                cbDilers.Enabled = true;
+                cbAll.Enabled = false;
+                cbUsed.Enabled = false;
+            }
+            else
+            {
+                cbDilers.Enabled = false;
+                cbHistory.Enabled = false;
+                cbCredit.Enabled = false;
+                cbAll.Enabled = true;
+                cbUsed.Enabled = true;
+            }
+        }
+
+        private void CbUsed_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbUsed.Checked == true)
+            {
+                cbAll.Enabled = false;
+                cbNew.Enabled = false;
+                cbDilers.Enabled = false;
+                cbHistory.Enabled = true;
+                cbCredit.Enabled = true;
+            }
+            else
+            {
+                cbAll.Enabled = true;
+                cbNew.Enabled = true;
+                cbDilers.Enabled = false;
+                cbHistory.Enabled = false;
+                cbCredit.Enabled = false;
+            }
+        }
+
+        private void CbAll_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbAll.Checked == true)
+            {
+                cbNew.Enabled = false;
+                cbUsed.Enabled = false;
+                cbHistory.Enabled = true;
+                cbCredit.Enabled = true;
+            }
+            else
+            {
+                cbNew.Enabled = true;
+                cbUsed.Enabled = true;
+                cbHistory.Enabled = false;
+                cbCredit.Enabled = false;
+            }
+        }
+
+        private void BtnClearLog_Click(object sender, EventArgs e)
+        {
+            rtbLog.Clear();
+        }
+        #endregion
+
+        HttpRequest danni = new HttpRequest();
+        CookieDictionary cookies = new CookieDictionary();
+        CsvExport myExport = new CsvExport();
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Thread main = new Thread(MainMethod);
+            main.Start();
+        }
+
+        private void MainMethod()
+        {
+            int startPage = 1;  //С какой страницы начать
+            int maxPage = 5;    //До какой страницы парсить
+            int timeBefore = 10000;
+
+            for (int q = startPage; q < maxPage; q++)
+            {
+                string url = "";
+
+                Invoke((MethodInvoker)delegate
+                {
+                    if (cbAll.Checked == true)
+                    {
+                        url = "https://auto.ru/cars/all/?sort=fresh_relevance_1-desc";
+
+                        if (cbHistory.Checked == true)
+                            url = "https://auto.ru/cars/all/?sort=fresh_relevance_1-desc&has_history=true";
+
+                        else if (cbCredit.Checked == true)
+                            url = "https://auto.ru/cars/all/?sort=fresh_relevance_1-desc&on_credit=true";
+
+                        else if (cbHistory.Checked == true && cbCredit.Checked == true)
+                            url = "https://auto.ru/cars/all/?has_history=true&sort=fresh_relevance_1-desc&on_credit=true";
+                    }
+
+                    else if (cbNew.Checked == true)
+                    {
+                        url = "https://auto.ru/cars/new/?sort=fresh_relevance_1-desc";
+
+                        if (cbDilers.Checked == false)
+                            url = "https://auto.ru/cars/new/?sort=fresh_relevance_1-desc&only_official=false";
+                    }
+
+                    else if (cbUsed.Checked == true)
+                    {
+                        url = "https://auto.ru/cars/used/?sort=fresh_relevance_1-desc";
+
+                        if (cbHistory.Checked == true)
+                            url = "https://auto.ru/cars/used/?sort=fresh_relevance_1-desc&has_history=true";
+
+                        else if (cbCredit.Checked == true)
+                            url = "https://auto.ru/cars/used/?sort=fresh_relevance_1-desc&on_credit=true";
+
+                        else if (cbHistory.Checked == true && cbCredit.Checked == true)
+                            url = "https://auto.ru/cars/used/?has_history=true&sort=fresh_relevance_1-desc&on_credit=true";
+                    }
+                });
+
+                url = url + $"&page={q}";
+
+                var linkes = GetLinkes(url);
+
+                var info = GetInfo(linkes);
+
+                int linkesScore = 0;
+
+                for (int i = 0; i < info.Count; i++)
+                {
+                    try
+                    {
+                        SaveInfo(linkes[linkesScore], info[i], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++], info[i++]);
+                        linkesScore++;
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+
+                myExport.ExportToFile("Cars.csv");
+
+                Thread.Sleep(timeBefore); //Задерджка перед парсом след страницы
+            }
+        }
+
+        private List<string> GetLinkes(string url)
+        {
+            List<string> href = new List<string>();
+
+            danni.KeepAlive = true;
+            danni.Cookies = cookies;
+            danni.UserAgent = Http.ChromeUserAgent();
+
+            var head = danni.Get(url);
+
+            if (head.Address.ToString().Contains("showcaptcha"))
+            {
+                string html = danni.Get(url).ToString();
+
+                var keys = GetKey(html);
+
+                string[] values = keys[0].Split('_');
+
+                string captcha = GetAnswer(values[0]);
+
+                keys[0] = keys[0].Replace("/", "%2F");
+
+                try
+                {
+                    var skipCaptcha = danni.Get($"https://auto.ru/checkcaptcha?key={keys[0]}&retpath={keys[1]}&rep={captcha}");
+
+                    if (!skipCaptcha.Address.ToString().Contains("https://auto.ru/cars"))
+                        GetLinkes(url);
+                }
+                catch
+                {
+                    GetLinkes(url);
+                }
+            }
+
+            string response = danni.Get(url).ToString();
+            string getCookies = danni.Get("https://matchid.adfox.yandex.ru/getcookie").ToString();
+
+            var parser = new HtmlParser();
+            var doc = parser.Parse(response);
+
+            var linkes = doc.QuerySelectorAll("a.ListingItemTitle-module__link");
+
+            foreach (var item in linkes)
+            {
+                href.Add(item.GetAttribute("href"));
+            }
+
+            return href;
+        }
+
+        private List<string> GetInfo(List<string> linkes)
+        {
+            List<string> info = new List<string>();
+
+            danni.KeepAlive = true;
+            danni.Cookies = cookies;
+            danni.UserAgent = Http.ChromeUserAgent();
+
+            for (int i = 0; i < linkes.Count(); i++)
+            {
+                string response = danni.Get(linkes[i]).ToString();
+
+                var parser = new HtmlParser();
+                var doc = parser.Parse(response);
+
+                string city = "";
+
+                try
+                {
+                    city = doc.QuerySelector("span.MetroListPlace__regionName").TextContent;
+                }
+                catch
+                {
+                    city = "Не указан";
+                }
+
+                info.Add(city);
+
+                var information = doc.QuerySelectorAll("span.CardInfo__cell");
+
+                int check = 0;
+
+                foreach (var item in information)
+                {
+                    if (check == 0)
+                    {
+                        check++;
+                        continue;
+                    }
+
+                    string text = item.TextContent;
+
+                    info.Add(text);
+                    check = 0;
+                }
+            }
+
+            return info;
+        }
+
+        private void SaveInfo(string link, string city, string year, string probeg, string kuzov, string color, string dvigatel, string box, string privod, string rul,
+            string status, string vladelec, string ptc, string tamoznya, string vin, string gosnomer)
+        {
+            myExport.AddRow();
+            myExport["Ссылка"] = link;
+            myExport["Город"] = city;
+            myExport["Год выпуска"] = year;
+            myExport["Пробен"] = probeg;
+            myExport["Кузов"] = kuzov;
+            myExport["Цвет"] = color;
+            myExport["Двигатель"] = dvigatel;
+            myExport["Коробка"] = box;
+            myExport["Привод"] = privod;
+            myExport["Руль"] = rul;
+            myExport["Состояние"] = status;
+            myExport["Владельцы"] = vladelec;
+            myExport["ПТС"] = ptc;
+            myExport["Таможня"] = tamoznya;
+            myExport["VIN"] = vin;
+            myExport["Госномер"] = gosnomer;
+        }
+
+        private string GetAnswer(string key)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile($"https://ext.captcha.yandex.net/image?key={key}", "captcha.jpg");
+            }
+
+            Rucaptcha.Key = "80772be92ec6b923a46970078c3ec829";
+            var cap = Rucaptcha.Recognize("captcha.jpg");
+
+            return cap;
+        }
+
+        private List<string> GetKey(string html)
+        {
+            List<string> keys = new List<string>();
+
+            var parser = new HtmlParser();
+            var doc = parser.Parse(html);
+
+            var key = doc.QuerySelector("input.form__key").GetAttribute("value");
+            var retpath = doc.QuerySelector("input.form__retpath").GetAttribute("value");
+
+            keys.Add(key);
+            keys.Add(retpath);
+
+            return keys;
+        }
+    }
+}
